13eb5ba029a8d00e6a13b1ab58d4a12a5db3502e
syd333 <sydmiranda@icloud.com>
2021-03-20 19:27:12 -0700

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ProgressPlugin } from 'webpack';
 
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
@@ -7,7 +8,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={console.log('whattodohere')} />
   </div>,
   document.getElementById('root')
 );
